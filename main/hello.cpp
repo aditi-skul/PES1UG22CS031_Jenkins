54394affa3749a830c7bc1b88190863f2a43bbcd
aditi-skul <82603068+aditi-skul@users.noreply.github.com>
2025-03-13 15:12:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "this is SRN: PES1UG22CS031\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
